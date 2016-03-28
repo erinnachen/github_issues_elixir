b6439931a6611d6b3cbc720da546fa286949d7f0
Erinna Chen <erinna.chen@gmail.com>
2016-03-28 08:11:48 -0600

    Parse reponses and sort into ascending order

--- lib/github_issues_elixir.ex
+++ lib/github_issues_elixir.ex
@@ -1,2 +1,7 @@
 defmodule GithubIssuesElixir do
+  @github_url Application.get_env(:issues, :github_url)
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
 end
